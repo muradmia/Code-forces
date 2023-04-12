db8cf4ab23bf5af7088d4c79ece0ea05a567af12
murad-mia <muradmia901@gmail.com>
2023-04-12 13:59:03 +0600

    Sheet#2 (Loop)

---
+++ Sheet#2 (Loop)/J.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int a,j;
+    scanf("%d",&a);
+
+   
+        for(j=2; j<a;j++){
+            printf("%d \n",j);
+            if(j % 2 != 0){
+               
+            }
+        }
+    return 0;
+}
+
