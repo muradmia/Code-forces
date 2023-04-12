b19daaf63f36444441835d9e1ad05009f8a6356d
murad-mia <muradmia901@gmail.com>
2023-04-12 14:01:56 +0600

    Sheet#2 (Loop)

---
+++ Sheet#2 (Loop)/Q.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+    int n,t;
+    scanf("%d",&t);
+
+    for(int i = 0;i < t;i++){
+        scanf("%d",&n);
+
+        do{
+
+            printf("%d ",n%10);
+            n = n / 10;
+
+        }while(n != 0);
+        printf("\n");
+
+    }
+    
+    return 0;
+}
