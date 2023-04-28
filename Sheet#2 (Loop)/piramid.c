4ff308505d0b93560895cb1ecbea2955f54e4a69
murad-mia <muradmia901@gmail.com>
2023-04-28 13:56:17 +0600

    Sheet#2 (Loop)

---
+++ Sheet#2 (Loop)/piramid.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+
+
+    int a;
+    scanf("%d",&a);
+
+    for(int i=1; i<=a; i++){
+
+        for(int j=1; j<=a-i; j++)
+        printf(" ");
+
+        for(int k=1; k<=2*i-1; k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for(int i = a-1;i >= 0;i--){
+        for(int j = a-i;j >= 1;j--){
+            printf(" ");
+        }
+
+        for(int k = 2*i-1; k >= 1; k--){
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+
+    return 0;
+}
