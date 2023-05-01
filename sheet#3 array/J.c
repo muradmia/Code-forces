d7739625af2a3cec0389bccfdeb601cfcd7db675
murad-mia <muradmia901@gmail.com>
2023-05-01 10:20:19 +0600

    sheet#3 array

---
+++ sheet#3 array/J.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include <limits.h>
+int main(){
+
+    int n,count = 0;
+    scanf("%d",&n);
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+    int min = arr[0];
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+    }
+    
+    for(int i=0; i<n; i++){
+        if(arr[i] == min){
+            count++;
+        }  
+    }
+
+  
+
+   if(count % 2 == 0){
+        printf("Unlucky\n");
+   }else{
+        printf("Lucky\n");
+   }
+
+
+    return 0;
+}
