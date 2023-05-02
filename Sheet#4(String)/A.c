6a9c798e0fefa880294126b17a50fe5d8f8784cb
murad-mia <muradmia901@gmail.com>
2023-05-02 21:40:45 +0600

    code forces/Sheet#4(String)

--- Sheet#4(String)/A.c
+++ Sheet#4(String)/A.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
 
+    char s[10001];
+    char t[10001];
 
-    printf("Hello world\n");
+    gets(s);
+    gets(t);
+
+    int len = strlen(s);
+    int len2 = strlen(t);
+
+    printf("%d %d",len,len2);
+    printf("\n%s %s",s,t);
 
     return 0;
 }
